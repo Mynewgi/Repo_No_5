77581159b0889f205b4db9085ecbbd29007c3dd8
Mynewgi <mina.m.monsef.apr@gmail.com>
2022-12-08 20:13:35 -0500

    first commit

---
+++ src/test/java/automation_code_assignment_5/FCPS_WebSite.java
@@ -0,0 +1,21 @@
+package automation_code_assignment_5;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FCPS_WebSite {
+
+	public static void main(String[] args) {
+
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.fcps.edu/");
+		
+
+	}
+
+}
